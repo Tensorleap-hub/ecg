d1a398f647cdc5bef3c7b143a5a81215ef33f9f5
Chen Rothschild <chen.rothschild@tensorleap.ai>
2024-02-29 12:14:09 +0200

    add data file

---
+++ ecg/data/preprocessing.py
@@ -0,0 +1,11 @@
+import wfdb
+import numpy as np
+
+
+def load_raw_data(df, sampling_rate, path):
+    if sampling_rate == 100:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_lr]
+    else:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_hr]
+    data = np.array([signal for signal, meta in data])
+    return data
